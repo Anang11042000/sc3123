0993bc73f23d92a644f755e1e86fdf8f8cf7904e
Anang11042000 <anangq928@gmail.com>
2023-08-06 22:30:53 +0700

    dancokkk

---
+++ menu/pubkey.sh
@@ -0,0 +1,17 @@
+NS_DOMAIN=$(cat /etc/xray/nsdomain)
+clear
+	wget -O dnstt-server "https://github.com/Anang11042000/sc3123/main/slowdns/dnstt-server" >/dev/null 2>&1
+	chmod +x dnstt-server >/dev/null 2>&1
+	wget -O dnstt-client "https://github.com/Anang11042000/sc3123/main/slowdns/dnstt-client" >/dev/null 2>&1
+	chmod +x dnstt-client >/dev/null 2>&1
+	./dnstt-server -gen-key -privkey-file server.key -pubkey-file server.pub
+	chmod +x *
+	mv * /etc/slowdns
+	wget -O /etc/systemd/system/client.service "https://github.com/Anang11042000/sc3123/main/slowdns/client" >/dev/null 2>&1
+	wget -O /etc/systemd/system/server.service "https://github.com/Anang11042000/sc3123/main/slowdns/server" >/dev/null 2>&1
+	sed -i "s/xxxx/$NS_DOMAIN/g" /etc/systemd/system/client.service 
+	sed -i "s/xxxx/$NS_DOMAIN/g" /etc/systemd/system/server.service
+	echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo "SUCCSES Renew Pubkey & Private Key DNS TUNNEL"
+read -n 1 -s -r -p "Press any key to back menu"
+menu
